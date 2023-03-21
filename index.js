9516d4d4d791ef82c2370cb4deb8acab524d1051
Mahamudul Hasan Ridoy <84871157+ridoy-mahmud@users.noreply.github.com>
2023-03-21 20:08:13 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { MongoClient } = require('mongodb');
 const express = require('express');
 require('dotenv').config()
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const cors = require('cors')
 const ObjectId = require('mongodb').ObjectId;
 app.use(cors());
